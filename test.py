1f20e7080638818c0a58c9387be8d3c29742609c
nieznanysprawiciel <nieznany.sprawiciel@gmail.com>
2016-11-07 21:28:38 +0100

    Test adaptive thresholding

---
+++ test.py
@@ -0,0 +1,32 @@
+import cv2
+import os
+
+#print "Working directory: "
+#print os.getcwd()
+
+
+imagesDir = "images/"
+thresholdDir = "thresh/"
+filePath = imagesDir + "auto000.jpg"
+
+images = os.listdir( imagesDir )
+
+if not os.path.exists( thresholdDir ):
+	os.makedirs( thresholdDir )
+	print "Created directory: " + thresholdDir
+
+for image in images:
+	filePath = imagesDir + image
+
+	if os.path.isfile( filePath ):
+		img = cv2.imread( filePath, 0 )
+		threshholding = cv2.adaptiveThreshold( img, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 11, 2 )
+
+		#cv2.imshow( filePath, threshholding )
+		print "Image thresholding result in: " + thresholdDir + image
+		cv2.imwrite( thresholdDir + image, threshholding )
+		
+		#cv2.waitKey(0)
+		#cv2.destroyAllWindows()
+	else:
+		print "Image doesn't exist: " + filePath
