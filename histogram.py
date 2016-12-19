f7eba180e566c7d38078a417b9b5175de23aa055
nieznanysprawiciel <nieznany.sprawiciel@gmail.com>
2016-12-19 22:11:28 +0100

    histogram segmentation first step

--- histogram.py
+++ histogram.py
@@ -27,18 +27,42 @@ def histograms( image ):
     return horizontal, vertical
     
     
+
+def histogram_segmentation( image ):
+    rows = image.shape[ 0 ]
+    columns = image.shape[ 1 ]
+    
+    search_threshold = rows * 0.1
+    idx_threshold = 2
+    print "search_threshold " + str( search_threshold )
+    
+    horizontal, vertical = histograms( image )
+    
+    
+    segments = [ idx for idx in range( len( horizontal ) )
+                if horizontal[ idx ] < search_threshold ]
+    
+    filtered_segments = [ [ segments[ idx ], segments[ idx + 1 ] ] for idx in range( len( segments ) - 1 )
+                        if segments[ idx + 1 ] - segments[ idx ] >= idx_threshold ]
+    
+    return filtered_segments
+    
+    
     
 def test():
     colorImage = cv2.imread( "purePlates/plate018.jpg", 0 )
     
     horizontal, vertical = histograms( colorImage )
     
-    print horizontal
-    print vertical
+    #print horizontal
+    #print vertical
     
     plt.plot( horizontal )
     plt.show()
-    plt.plot( vertical )
-    plt.show()
+    #plt.plot( vertical )
+    #plt.show()
+    
+    segments = histogram_segmentation( colorImage )
+    print segments
     
 test()
